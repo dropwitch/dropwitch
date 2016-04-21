9b56e647b7c5cab108805ba2d676d0984bb945ce
shun-tak <shun.tak221@gmail.com>
2016-04-21 19:26:28 +0900

    MessagePackFactory doesn't work

--- src/main/java/com/github/dropwitch/api/ResponseBody.java
+++ src/main/java/com/github/dropwitch/api/ResponseBody.java
@@ -1,5 +1,7 @@
 package com.github.dropwitch.api;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,4 +13,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class ResponseBody {
     private boolean result;
     private Object data;
+
+    @JsonCreator
+    ResponseBody(@JsonProperty("result") boolean result, @JsonProperty("data") Object data) {
+        this.result = result;
+        this.data = data;
+    }
 }
---
+++ src/test/java/com/github/dropwitch/api/ResponseBodyTest.java
@@ -0,0 +1,68 @@
+package com.github.dropwitch.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
+import org.junit.Test;
+import org.msgpack.jackson.dataformat.MessagePackFactory;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ResponseBodyTest {
+    private static final ObjectMapper JSON_MAPPER = Jackson.newObjectMapper();
+    private static final ObjectMapper MSGPACK_MAPPER = Jackson.newObjectMapper(new MessagePackFactory());
+
+    @Test
+    public void serializeToJson() throws Exception {
+        final ResponseBody body = ResponseBody
+                .builder()
+                .result(true)
+                .data(null)
+                .build();
+
+        final String expected = JSON_MAPPER.writeValueAsString(
+                JSON_MAPPER.readValue(fixture("fixtures/api/response_body.json"), ResponseBody.class));
+
+        assertThat(JSON_MAPPER.writeValueAsString(body)).isEqualTo(expected);
+    }
+
+    @Test
+    public void deserializeFromJson() throws Exception {
+        final ResponseBody body = JSON_MAPPER.readValue(fixture("fixtures/api/response_body.json"), ResponseBody.class);
+
+        final ResponseBody expected = ResponseBody
+                .builder()
+                .result(true)
+                .data(null)
+                .build();
+
+        assertThat(body).isEqualTo(expected);
+    }
+
+    @Test
+    public void serializeToMsgPack() throws Exception {
+        final ResponseBody body = ResponseBody
+                .builder()
+                .result(true)
+                .data(null)
+                .build();
+
+        final String expected = MSGPACK_MAPPER.writeValueAsString(
+                MSGPACK_MAPPER.readValue(fixture("fixtures/api/response_body.msgpack"), ResponseBody.class));
+
+        assertThat(MSGPACK_MAPPER.writeValueAsString(body)).isEqualTo(expected);
+    }
+
+    @Test
+    public void deserializeFromMsgPack() throws Exception {
+        final ResponseBody body = MSGPACK_MAPPER.readValue(fixture("fixtures/api/response_body.msgpack"), ResponseBody.class);
+
+        final ResponseBody expected = ResponseBody
+                .builder()
+                .result(true)
+                .data(null)
+                .build();
+
+        assertThat(body).isEqualTo(expected);
+    }
+}
